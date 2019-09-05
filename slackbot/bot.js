d41474b81774f428588c880d6a76379acf8eba7a
Jonas Ahrens <jonas.ahrens@gmail.com>
2019-09-05 21:29:07 +0200

    Format code

--- slackbot/bot.js
+++ slackbot/bot.js
@@ -1,21 +1,21 @@
 require('dotenv').config()
-const { RTMClient } = require('@slack/rtm-api');
+const { RTMClient } = require('@slack/rtm-api')
 
-const token = process.env.SLACK_BOT_TOKEN;
+const token = process.env.SLACK_BOT_TOKEN
 
-const rtm = new RTMClient(token);
+const rtm = new RTMClient(token)
 
-rtm.on('message', async (event) => {
-  if (event.subtype || !(event.channel.startsWith('C'))) return
-  console.log(event);
+rtm.on('message', async event => {
+  if (event.subtype || !event.channel.startsWith('C')) return
+  console.log(event)
   const reply = await rtm.sendMessage(`Ayyy, <@${event.user}>`, event.channel)
-});
-
+})
+;
 (async () => {
   // Connect to Slack
-  const { self, team } = await rtm.start();
+  const { self, team } = await rtm.start()
   console.log('Connected!')
   setTimeout(() => {
     console.log(self, team)
-  }, 10000);
-})();
+  }, 5000)
+})()
