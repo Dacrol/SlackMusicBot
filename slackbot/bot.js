7267f0d9b1643a4630a3a9698c30d40f9f1c3231
Jonas Ahrens <jonas.ahrens@gmail.com>
2019-09-05 21:52:56 +0200

    Play YouTube URLs

--- slackbot/bot.js
+++ slackbot/bot.js
@@ -1,17 +1,32 @@
 require('dotenv').config()
 const { RTMClient } = require('@slack/rtm-api')
+const Player = require('../player/player')
+const player = new Player()
 
 const token = process.env.SLACK_BOT_TOKEN
 
 const rtm = new RTMClient(token)
 
+const urlRegex = /<(.+)>/
+
 rtm.on('message', async event => {
-  if (event.subtype || !event.channel.startsWith('C')) return
+  if (event.subtype || !event.channel.startsWith('C')) {
+    return
+  }
   console.log(event)
   const reply = await rtm.sendMessage(`Ayyy, <@${event.user}>`, event.channel)
+  const url = urlRegex.exec(event.text)
+  console.log(url)
+  if (!Array.isArray(url) || typeof url[1] !== 'string') {
+    return
+  }
+  try {
+    await player.play(url[1])
+  } catch (error) {
+    console.warn(error)
+  }
 })
-;
-(async () => {
+;(async () => {
   // Connect to Slack
   const { self, team } = await rtm.start()
   console.log('Connected!')
