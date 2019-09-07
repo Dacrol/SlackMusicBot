86a2690fea98553066c9cb0212d5c89edb52c298
Jonas Ahrens <jonas.ahrens@gmail.com>
2019-09-07 15:40:37 +0200

    Add ALSA installation

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cp .asoundrc ~/.asoundrc
-sudo apt-get install ffmpeg libasound2-dev mkchromecast mkchromecast-alsa -y
+sudo apt-get install ffmpeg libasound2-dev libasound2 alsa-utils alsa-oss mkchromecast mkchromecast-alsa -y
 sudo bash -c 'echo "snd-aloop" >> /etc/modules'
