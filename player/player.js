70401d196d4e7cd61affe053a0074c81764e7f82
Jonas Ahrens <jonas.ahrens@gmail.com>
2019-09-08 20:13:45 +0200

    Add player volume control

--- player/player.js
+++ player/player.js
@@ -1,7 +1,10 @@
-const decoder = require('lame').Decoder
+const lame = require('lame')
+const Decoder = lame.Decoder
 const Speaker = require('speaker')
 const ytdl = require('ytdl-core')
 const ffmpeg = require('fluent-ffmpeg')
+const Volume = require('pcm-volume')
+const volume = new Volume()
 
 class Player {
   constructor() {
@@ -9,38 +12,40 @@ class Player {
   }
 
   set volume(vol) {
+    if (vol > 100) {
+      vol = 100
+    }
+    if (vol > 1) {
+      vol = vol / 100
+    }
+    volume.setVolume(vol)
   }
 
   play(url) {
     return new Promise(async (resolve, reject) => {
-      if (
-        !(
-          typeof url === 'string' &&
-          ['https://www.youtube.com/', 'http://www.youtube.com/', 'youtube.com/', 'http://youtube.com/', 'https://youtube.com/'].some(validUrl => {
-            return url.startsWith(validUrl)
-          })
-        )
-      ) {
-        return
-      }
       try {
         const stream = ytdl(url, {
-          // highWaterMark: 2 ** 25,
+          highWaterMark: 2 ** 25,
           quality: 'highestaudio',
           filter: 'audioonly'
         })
 
         const audio = ffmpeg(stream).format('mp3')
-
+        let time;
         // @ts-ignore
         const speaker = new Speaker({
           channels: 2,
           bitDepth: 16,
-          sampleRate: 44100
+          sampleRate: 48000
         })
 
         // @ts-ignore
-        const playing = audio.pipe(decoder()).pipe(speaker)
+        const playing = audio.pipe(new Decoder({
+          channels: 2,
+          bitDepth: 16,
+          sampleRate: 48000,
+          mode: lame.STEREO
+        })).pipe(volume).pipe(speaker)
         playing.on('close', () => {
           console.log('Audio played successfully')
           resolve()
@@ -52,6 +57,9 @@ class Player {
   }
 
   queue(url) {
+    if (!isYoutubeUrl(url)) {
+      return
+    }
     this.playQueue.push(url)
     if (this.playQueue.length === 1) {
       this.playNext()
@@ -67,9 +75,22 @@ module.exports = Player
 if (process.argv[2]) {
   const player= new Player()
   player.play(process.argv[2]).then(() => {
-    console.log('Audio played successfully')
     setTimeout(() => {
       process.exit(0)
     }, 1000);
   })
+}
+
+function isYoutubeUrl(url) {
+  if (
+    (
+      typeof url === 'string' &&
+      ['https://www.youtube.com/', 'http://www.youtube.com/', 'youtube.com/', 'http://youtube.com/', 'https://youtube.com/'].some(validUrl => {
+        return url.startsWith(validUrl)
+      })
+    )
+  ) {
+    return true
+  }
+  return false
 }
