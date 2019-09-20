051fd323e054c83889ce15663d6759df2289eb2b
Jonas Ahrens <jonas.ahrens@gmail.com>
2019-09-20 22:05:16 +0200

    Convert sample rate

--- player/player.js
+++ player/player.js
@@ -56,7 +56,9 @@ class Player {
 
         this.currentStream = stream
 
-        const audio = ffmpeg(stream).format('mp3')
+        const audio = ffmpeg(stream).format('mp3').inputOptions([
+          '-af asetrate=48000'
+        ])
         audio.on('error', (error) => {
           streamError = error
           reject({error: error, url: url, info: info, queueItem: next})
